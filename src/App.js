18aa426189f1bc6bb3fef49aa7950947a484ab9e
Luis Galvan <luismgc.001@hotmail.com>
2022-11-23 17:11:04 -0500

    Local Storage Set

--- src/App.js
+++ src/App.js
@@ -2,15 +2,26 @@ import React from 'react';
 import { AppUI } from './AppUI';
 // import './App.css';
 
-const defaultTodos = [
-  { text: 'Cortar cebolla', completed: true },
-  { text: 'Tomar el cursso de intro a React', completed: false },
-  { text: 'Llorar con la llorona', completed: false },
-  { text: 'LALALALAA', completed: false },
-];
+// const defaultTodos = [
+//   { text: 'Cortar cebolla', completed: true },
+//   { text: 'Tomar el cursso de intro a React', completed: false },
+//   { text: 'Llorar con la llorona', completed: false },
+//   { text: 'LALALALAA', completed: false },
+// ];
 
 function App() {
-  const [todos, setTodos] = React.useState(defaultTodos);
+  const localStorageTodos = localStorage.getItem("TODOS_V1")
+  let parsedTodos;
+
+  if(!localStorageTodos){
+    localStorage.setItem("TODOS_V1",JSON.stringify([]))
+    parsedTodos=[];
+
+  }else{
+    parsedTodos = JSON.parse(localStorageTodos);
+  }
+
+  const [todos, setTodos] = React.useState(parsedTodos);
   const [searchValue, setSearchValue] = React.useState("");
   const completedTodos = todos.filter(todo => !!todo.completed).length;
   const totalTodos = todos.length;
@@ -29,6 +40,14 @@ function App() {
     });
     
   }
+  const saveTodos = (newTodos) => {
+    const stringifiedTodos= JSON.stringify(newTodos)
+
+    localStorage.setItem("TODOS_V1",stringifiedTodos);
+    setTodos(newTodos);
+
+
+  }
 
   const completeTodos = (text) => {
     const todoIndex=todos.findIndex(todo => todo.text == text);
@@ -39,7 +58,8 @@ function App() {
     //   text: todos[todoIndex].text,
     //   completed: true;
     // };
-    setTodos(newTodos);
+    saveTodos(newTodos);
+    //localStorage.setItem("TODOS_V1",JSON.stringify(newTodos))
 
   }
 
@@ -52,7 +72,8 @@ function App() {
     //   text: todos[todoIndex].text,
     //   completed: true;
     // };
-    setTodos(newTodos);
+    saveTodos(newTodos);
+    //localStorage.setItem("TODOS_V1",JSON.stringify(newTodos))
 
   }
 
