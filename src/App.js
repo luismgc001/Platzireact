734c4012496d42e4e878be0333078c5f614e6cf9
luismgc001 <110427411+luismgc001@users.noreply.github.com>
2022-11-22 20:38:18 -0500

    filter function added

--- src/App.js
+++ src/App.js
@@ -19,6 +19,21 @@ function App() {
   const completedTodos = todos.filter(todo => !!todo.completed).length;
   const totalTodos = todos.length;
 
+  let searchedTodos = [];
+ 
+  if (!searchValue.length >= 1){
+    searchedTodos = todos;
+
+  }else{
+    searchedTodos = todos.filter(todo => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText);
+
+    });
+    
+  }
+
 
   return (
     <React.Fragment>
@@ -35,7 +50,7 @@ function App() {
       />      
 
       <TodoList>
-        {todos.map(todo => (
+        {searchedTodos.map(todo => (
           <TodoItem
             key={todo.text}
             text={todo.text}
